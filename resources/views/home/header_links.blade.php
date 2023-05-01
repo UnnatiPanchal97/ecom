633de114f153d7b9371291e80f77a7bb53cdf803
Unnati <unnati@topsinfosolutions.com>
2023-05-01 10:38:52 +0530

    "css not found issue resolve"

--- resources/views/home/header_links.blade.php
+++ resources/views/home/header_links.blade.php
@@ -1,7 +1,7 @@
 <link rel="shortcut icon" href="{{ asset('/home/images/favicon.png') }}" type="">
 <title>Famms - Fashion HTML Template</title>
 <!-- bootstrap core css -->
-<link rel="stylesheet" type="text/css" href="{{ '/home/css/bootstrap.css' }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('/home/css/bootstrap.css') }}" />
 <!-- font awesome style -->
 <link href="{{ asset('/home/css/font-awesome.min.css') }}" rel="stylesheet" />
 <!-- Custom styles for this template -->
