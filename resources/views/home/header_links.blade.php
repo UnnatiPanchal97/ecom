c4b7509a130298e83b27f7ed56fb1cd7b684c048
Unnati <unnati@topsinfosolutions.com>
2023-05-09 12:37:57 +0530

    "changes update"

--- resources/views/home/header_links.blade.php
+++ resources/views/home/header_links.blade.php
@@ -1,5 +1,5 @@
 <link rel="shortcut icon" href="{{ asset('/home/images/favicon.png') }}" type="">
-<title>Famms - Fashion HTML Template</title>
+<title>Famms</title>
 <!-- bootstrap core css -->
 <link rel="stylesheet" type="text/css" href="{{ asset('/home/css/bootstrap.css') }}" />
 <!-- font awesome style -->
